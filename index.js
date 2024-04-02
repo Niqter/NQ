52be6cc03b0b4078d47778d1be3d2b63891893f3
NAQIU <naqiuudin03@gmail.com>
2024-04-02 10:44:20 +0800

    crud op

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.listen(port, () => {
    console.log(`Example app listening on port ${port}`)
 })
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://naqiuudin03:naqiu@cluster0.boapn52.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +31,42 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    //insert one
+    //  let result = await client.db('testing').collection('subject').insertOne({
+    //    subject:'Berg 123',
+    //    code:'Berg 123',
+    //    credit:2
+
+
+
+    //  })
+    
+    // console.log(result)
+
+   //find
+   // let subject = await client.db('testing').collection('subject').find().toArray()
+   // console.log(subject)
+
+  //updatedone
+    // let updated= await client.db('testing').collection('subject').updateOne(
+    //   {code:'Berr 123'},
+    //   {
+    //   $set:{
+    //     description:'Data science',
+    //     lecturer:'Dr john',
+    //     semester:3
+    //   }
+
+    // }
+    // )
+    // console.log(updated)
+
+    //deleteone
+    let deleted=await client.db('testing').collection('subject').deleteOne()
+    {
+      _id: new ObjectId('660b6db5fc1e24180f921e22')
+
+    }
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
